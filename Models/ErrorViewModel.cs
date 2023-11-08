90c5104e14067e0f23d2745d4d72624693eaf458
Mikita Andrushkevich <Mikita.Andrushkevich@ventionteams.com>
2023-11-08 12:11:19 +0200

    Initializing solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_webApi_template.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
